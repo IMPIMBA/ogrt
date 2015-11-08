f355d5f4420c8f14f00cb79edb8ca9ba3c36ff99
Georg Rath <rath.georg@gmail.com>
2015-11-08 16:06:49 +0100

    File output is still broken, but now outputs valid gv format.

--- ogrt-server/output/file.go
+++ ogrt-server/output/file.go
@@ -16,11 +16,25 @@ func (fw *FileWriter) Open() {
 	if err != nil {
 		panic(err)
 	}
+
+	if _, err := fw.file.WriteString("digraph ogrt {\n\tnode[shape=record]\n"); err != nil {
+		panic(err)
+	}
 }
 
 func (fw *FileWriter) Persist(pid int64, parentPid int64, msg_type string, exec string) {
-	out := fmt.Sprintf("%d -> %d;\n%d[label=\"{%d|%s}\"];\n", parentPid, pid, pid, pid, exec)
+	out := fmt.Sprintf("\t%d -> %d [label=\"exec\"];\n\t%d[label=\"{%d|%s}\"];\n", parentPid, pid, pid, pid, exec)
 	if _, err := fw.file.WriteString(out); err != nil {
+		panic(err)
+	}
+}
+
+func (fw *FileWriter) Close() {
+	if _, err := fw.file.WriteString("}"); err != nil {
+		panic(err)
+	}
+
+	if err := fw.file.Close(); err != nil {
 		panic(err)
 	}
 }
