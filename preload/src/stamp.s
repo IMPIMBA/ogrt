22c4479f5c2eeeaf9535d2cc1a4c15972319de9d
Georg Rath <rath.georg@gmail.com>
2015-12-07 15:25:22 +0100

    stamp now has right name

--- preload/src/stamp.s
+++ preload/src/stamp.s
@@ -3,9 +3,9 @@
     .long 1f - 0f           # name size (not including padding)
     .long 3f - 2f           # desc size (not including padding)
     .long 0x4F475254        # type
-0:  .asciz "OG"     # name
+0:  .asciz "OGRT"     # name
 1:  .p2align 2
 2:  .byte  0x01 #version
-    .asciz "33db3477-1465-459b-a4d7-b74c46e8c2c1"        # uuid
+    .asciz "AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA"        # uuid
 3:  .p2align 2
 
