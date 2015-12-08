008bc2156ca37a3a4b6da309ec660525abdebb75
Georg Rath <rath.georg@gmail.com>
2015-12-08 23:27:57 +0100

    cleanup...

---
+++ preload/src/ogrt-cli.h
@@ -0,0 +1,8 @@
+#ifndef OGRT_CLI_H_INCLUDED
+#define OGRT_CLI_H_INCLUDED
+
+#define _GNU_SOURCE
+
+#include "ogrt-main.h"
+
+#endif
--- preload/src/ogrt-cmdline.h
+++ preload/src/ogrt-cmdline.h
@@ -31,7 +31,7 @@ extern "C" {
 
 #ifndef CMDLINE_PARSER_VERSION
 /** @brief the program version */
-#define CMDLINE_PARSER_VERSION "adff-dirty"
+#define CMDLINE_PARSER_VERSION "a53e-dirty"
 #endif
 
 /** @brief Where the command line options are stored */
